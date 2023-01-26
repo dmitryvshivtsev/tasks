f9d4823f669d4b6ab232998abf67882e93e3653f
rosettel <rosettel@student.21-school.ru>
2023-01-26 22:48:22 +0300

    another two

--- challenges.py
+++ challenges.py
@@ -53,38 +53,105 @@ def cakes(recipe, available):
 #==========================================================================#
 
 '''
+= Calculating with Functions =
 
-Two sum
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+seven(times(five())) # must return 35
+four(plus(nine())) # must return 13
+eight(minus(three())) # must return 5
+six(divided_by(two())) # must return 3
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Requirements:
 
-You can return the answer in any order.
+* There must be a function for each number from 0 ("zero") to 9 ("nine")
+* There must be a function for each of the following mathematical operations: plus, minus, times, divided_by
+* Each calculation consist of exactly one operation and two numbers
+* The most outer function represents the left operand, the most inner function represents the right operand
+* Division should be integer division. For example, this should return 2, not 2.666666...:
 
- 
-
-Example 1:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
 '''
 
+def zero(func_oper=None): return int(func_oper(0)) if func_oper else 0
+def one(func_oper=None): return int(func_oper(1)) if func_oper else 1
+def two(func_oper=None): return int(func_oper(2)) if func_oper else 2
+def three(func_oper=None): return int(func_oper(3)) if func_oper else 3
+def four(func_oper=None): return int(func_oper(4)) if func_oper else 4
+def five(func_oper=None): return int(func_oper(5)) if func_oper else 5
+def six(func_oper=None): return int(func_oper(6)) if func_oper else 6
+def seven(func_oper=None): return int(func_oper(7)) if func_oper else 7
+def eight(func_oper=None): return int(func_oper(8)) if func_oper else 8
+def nine(func_oper=None): return int(func_oper(9)) if func_oper else 9
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        tmp_sum = 0
-        result = []
-        for i, num in enumerate(nums):
-            tmp_sum += num
-            result.append(i)
-            if tmp_sum == target:
-                return result
-            else:
-                if tmp_sum > target:
-                    tmp_sum -= result.pop(i - 1)
-                    if tmp_sum == target:
-                        return result
+def plus(func_num): return lambda x: x + func_num
+def minus(func_num): return lambda x: x - func_num
+def times(func_num): return lambda x: x * func_num
+def divided_by(func_num): return lambda x: x / func_num
+
+
+#==========================================================================#
+
+'''
+
+= Roman to integer =
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+
+Input: s = "LVIII"
+Output: 58
+
+'''
+
+def romanToInt(self, s: str) -> int:
+    roman_nums = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), 
+                 (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I'))
+    result = 0
+    for num, roman_num in roman_nums:
+        while s.startswith(roman_num):
+            result += num
+            s = s[len(roman_num):]
+    return result
+
+
+#==========================================================================#
+
+
+
+#==========================================================================#
+
+
+
+#==========================================================================#
+
+
+
+#==========================================================================#
+
+
+
+#==========================================================================#
