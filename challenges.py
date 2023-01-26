a8825ecbd8f185c3bc180d3f02a90f962a3e81e3
rosettel <rosettel@student.21-school.ru>
2023-01-26 22:47:50 +0300

    another two

--- challenges.py
+++ challenges.py
@@ -18,10 +18,7 @@ def max_sequence(arr):
         current += num
         if current < 0: current = 0
         if max_num < current: max_num = current
-    return max_num
-
-
-print(max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43]))    
+    return max_num 
 
 #==========================================================================#
 
@@ -43,10 +40,7 @@ cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flo
 '''
 
 def cakes(recipe, available):
-    recipe_list = recipe.items()
-    avail_list = available.items()
     res_list = []
-
     for elem in recipe_list:
         if elem[0] in available:
             avail_price = available.get(elem[0])
@@ -58,3 +52,39 @@ def cakes(recipe, available):
 
 #==========================================================================#
 
+'''
+
+Two sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+'''
+
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tmp_sum = 0
+        result = []
+        for i, num in enumerate(nums):
+            tmp_sum += num
+            result.append(i)
+            if tmp_sum == target:
+                return result
+            else:
+                if tmp_sum > target:
+                    tmp_sum -= result.pop(i - 1)
+                    if tmp_sum == target:
+                        return result
